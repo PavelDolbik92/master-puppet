7ccc3578b6ee53def0ed61e17135d693dc277154
PavelDolbik92 <138321615+PavelDolbik92@users.noreply.github.com>
2023-07-13 00:17:35 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,29 +4,3 @@ node default {
      enable => false,
    }
 }
-
-node slave1.puppet {
-  package { 'httpd':
-    ensure => 'installed',
-  }
-
-  service {
-     ensure => running,
-     enable => true,
-   }
-}
-
-node slave2.puppet {
-  package { 'httpd':
-    ensure => 'installed',
-  }
-
-  package { 'php':
-    ensure => 'installed',
-  }
-
-  service {
-     ensure => running,
-     enable => true,
-   }
-}
