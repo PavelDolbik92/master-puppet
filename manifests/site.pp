b16fdc29dd6662c315fa100ff2a4a5706cef3147
pavel.dolbik <pavel.dolbik92@gmail.com>
2023-07-16 19:05:28 +0300

    Выполнение домашнего задания 4

--- manifests/site.pp
+++ manifests/site.pp
@@ -19,7 +19,7 @@ node mineserver.puppet {
   java::download { 'jdk19' :
     ensure  => "present",
     java_se => "jdk",
-    url     => "https://download.java.net/java/GA/jdk19.0.2/fdb695a9d9064ad6b064dc6df578380c/7/GPL/openjdk-19.0.2_linux-x64_bin.tar.gz",
+    url     => "https://rpmfind.net/linux/openmandriva/cooker/repository/x86_64/main/release/java-19-openjdk-19.0.2.7-1-omv2390.x86_64.rpm",
   }
 }
 
