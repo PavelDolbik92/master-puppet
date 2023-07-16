ac967eb3011d2edaaa9648ee68d53df3b624bce5
pavel.dolbik <pavel.dolbik92@gmail.com>
2023-07-16 21:41:33 +0300

    Выполнение домашнего задания 4

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,34 @@
 node mineserver.puppet {
-  package { 'java-latest-openjdk.x86_64':
-  ensure => 'installed',
+  package { 'java':
+    name => 'java-latest-openjdk.x86_64'
+    ensure => 'installed',
     provider => 'dnf'
   }
-
-  file { '/opt/minecraft':
+  -> file { '/opt/minecraft':
     ensure => directory,
-    before => File['minecraft'],
   }
-
-  file { 'minecraft':
+  -> file { 'eula.txt':
+    path => "/opt/minecraft/eula.txt",
+    ensure => present,
+    source => "/vagrant/eula.txt",
+  }
+  -> file { 'minecraft':
     path => "/opt/minecraft/server.jar",
     source => "https://piston-data.mojang.com/v1/objects/84194a2f286ef7c14ed7ce0090dba59902951553/server.jar",
     mode => "755",
-    require => File['/opt/minecraft'],
   }
-
-  file { 'minecraft_service':
+  -> file { 'minecraft_service':
     path => "/etc/systemd/system/minecraft.service",
     ensure => present,
     source => "/vagrant/minecraft.service",
-    notify => Service["minecraft"]
   }
-
-  service { 'minecraft':
+  -> file { 'minecraft_service':
+    path => "/etc/systemd/system/minecraft.service",
+    ensure => present,
+    source => "/vagrant/minecraft.service",
+  }
+  ~> service { 'minecraft':
     ensure => running,
-    require => File["minecraft_service"]
-
   }
 }
 
